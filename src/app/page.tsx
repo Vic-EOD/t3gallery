283d953c07787af5ad8c4562237b6745def83771
Victor <victorpharmd@yahoo.com>
2024-04-18 22:10:19 -0400

    add auth for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
@@ -20,6 +20,21 @@ export default async function HomePage() {
           ))
         }
       </div>
+    </main>
+  );
+}
+
+export default async function HomePage() {
+
+
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="h-full w-full text-2xl">Please sign in above.</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
